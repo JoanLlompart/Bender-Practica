6e19ce5596ed73a2fa2e86553232cf5a9a1f37aa
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2023-02-20 16:46:16 +0100

    Base sense codi

---
+++ src/Bender.java
@@ -0,0 +1,18 @@
+public class Bender {
+    //Constructor
+    String mapa ="";
+    Bender(String mapa){
+
+    }
+
+    String run(){
+
+        return null;
+    };
+    int bestRun() {
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
